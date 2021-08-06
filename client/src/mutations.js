b9a76f8911df5244a00d54579ed1f6e9b597f8bf
surajverma2587 <surajverma2587@gmail.com>
2021-08-06 17:40:44 +0100

    connect app with apollo client and complete login and signup

---
+++ client/src/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Mutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation Mutation($addUserInput: AddUserInput!) {
+    addUser(input: $addUserInput) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
